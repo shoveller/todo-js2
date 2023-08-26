82cb0c097490d46a0fe26604cc0db66df2993bfc
cinos81 <cinos81@gmail.com>
2023-08-26 11:48:53 +0900

    안티패턴

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
+import { useRef, useState } from "react";
 import "./App.css";
 
 function App() {
+  const ref = useRef();
+  const [todoList, setTodoList] = useState([]);
+
+  const onClick = () => {
+    setTodoList([...todoList, ref.current.value]);
+  };
+  const onKeyUp = (e) => {
+    if (e.keyCode === 13) {
+      setTodoList([...todoList, ref.current.value]);
+    }
+  };
+
   return (
     <div className="flex items-center justify-center w-screen h-screen font-medium">
       <div className="flex flex-grow items-center justify-center bg-gray-900 h-full">
@@ -23,65 +36,72 @@ function App() {
             <h4 className="font-semibold ml-3 text-lg">Todo List</h4>
           </div>
           {/* todo[s] */}
-          <div className="group">
-            {/* todo label[s] */}
-            <label className="flex items-center h-10 px-2 rounded cursor-pointer hover:bg-gray-900">
-              {/* hidden check[s] */}
-              <input
-                name="hidden-check"
-                className="hidden peer"
-                type="checkbox"
-              />
-              {/* hidden check[e] */}
-              {/* check[s] */}
-              <span
-                name="check"
-                className="peer-checked:bg-green-500 peer-checked:border-green-500 peer-checked:text-white flex items-center justify-center w-5 h-5 text-transparent border-2 border-gray-500 rounded-full"
-              >
-                <svg
-                  className="w-4 h-4 fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                    clipRule="evenodd"
+          {todoList.map((todo, index) => {
+            const key = `todoList_${index}`;
+
+            return (
+              <div className="group" key={key}>
+                {/* todo label[s] */}
+                <label className="flex items-center h-10 px-2 rounded cursor-pointer hover:bg-gray-900">
+                  {/* hidden check[s] */}
+                  <input
+                    name="hidden-check"
+                    className="hidden peer"
+                    type="checkbox"
                   />
-                </svg>
-              </span>
-              {/* check[e] */}
-              {/* content[s] */}
-              <span
-                className="select-none mx-4 text-sm w-full peer-checked:line-through"
-                name="viewer"
-              >
-                Trim the verge.
-              </span>
-              {/* content[e] */}
-              {/* delete[s] */}
-              <button name="delete" className="hidden group-hover:block">
-                <svg
-                  className="w-5 h-5 text-gray-400 fill-current mx-1"
-                  version="1.1"
-                  xmlns="http://www.w3.org/2000/svg"
-                  xmlnsXlink="http://www.w3.org/1999/xlink"
-                  viewBox="0 0 27.965 27.965"
-                  xmlSpace="preserve"
-                >
-                  <path
-                    d="M13.98,0C6.259,0,0,6.261,0,13.983c0,7.721,6.259,13.982,13.98,13.982c7.725,0,13.985-6.262,13.985-13.982
+                  {/* hidden check[e] */}
+                  {/* check[s] */}
+                  <span
+                    name="check"
+                    className="peer-checked:bg-green-500 peer-checked:border-green-500 peer-checked:text-white flex items-center justify-center w-5 h-5 text-transparent border-2 border-gray-500 rounded-full"
+                  >
+                    <svg
+                      className="w-4 h-4 fill-current"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </span>
+                  {/* check[e] */}
+                  {/* content[s] */}
+                  <span
+                    className="select-none mx-4 text-sm w-full peer-checked:line-through"
+                    name="viewer"
+                  >
+                    {todo}
+                  </span>
+                  {/* content[e] */}
+                  {/* delete[s] */}
+                  <button name="delete" className="hidden group-hover:block">
+                    <svg
+                      className="w-5 h-5 text-gray-400 fill-current mx-1"
+                      version="1.1"
+                      xmlns="http://www.w3.org/2000/svg"
+                      xmlnsXlink="http://www.w3.org/1999/xlink"
+                      viewBox="0 0 27.965 27.965"
+                      xmlSpace="preserve"
+                    >
+                      <path
+                        d="M13.98,0C6.259,0,0,6.261,0,13.983c0,7.721,6.259,13.982,13.98,13.982c7.725,0,13.985-6.262,13.985-13.982
         C27.965,6.261,21.705,0,13.98,0z M19.992,17.769l-2.227,2.224c0,0-3.523-3.78-3.786-3.78c-0.259,0-3.783,3.78-3.783,3.78
         l-2.228-2.224c0,0,3.784-3.472,3.784-3.781c0-0.314-3.784-3.787-3.784-3.787l2.228-2.229c0,0,3.553,3.782,3.783,3.782
         c0.232,0,3.786-3.782,3.786-3.782l2.227,2.229c0,0-3.785,3.523-3.785,3.787C16.207,14.239,19.992,17.769,19.992,17.769z"
-                  />
-                </svg>
-              </button>
-              {/* delete[e] */}
-            </label>
-            {/* todo label[e] */}
-          </div>
+                      />
+                    </svg>
+                  </button>
+                  {/* delete[e] */}
+                </label>
+                {/* todo label[e] */}
+              </div>
+            );
+          })}
+
           {/* todo[e] */}
           {/* form[s] */}
           <div className="flex items-center w-full mt-2">
@@ -89,6 +109,7 @@ function App() {
             <button
               id="submit"
               className="h-8 px-2 text-sm font-medium rounded"
+              onClick={onClick}
             >
               <svg
                 className="w-5 h-5 text-gray-400 fill-current"
@@ -114,6 +135,8 @@ function App() {
               name="todo"
               required
               placeholder="add a new todo"
+              ref={ref}
+              onKeyUp={onKeyUp}
             />
             {/* todo input[e] */}
             {/* reset button[s] */}
